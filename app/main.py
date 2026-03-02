e0b774e6d3de6df3cf399fb9d771357ed1daa03a
Raikonif <raikonif@gmail.com>
2026-03-02 12:56:24 -0400

    feat: healthcheck implemented

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from app.api.healt_check import router as health_router
 from app.api.stripe_webhooks import router as webhook_router
 
 app = FastAPI(title="Stripe Webhook Service")
 
+app.include_router(health_router)
 app.include_router(webhook_router)
